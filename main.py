b742cee2afdbd1ff55beb1eb80d05ea6674ef013
kaushik-yadav <kaushiky005@gmail.com>
2025-07-12 23:57:09 +0530

    feat: Add main application code for fastAPI

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+import os
+
+import google.generativeai as genai
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from transcribe import transcribe_user_question
+from api.transcribe import router as transcribe_router
+from api.upload import router as upload_router
 
 app = FastAPI()
+
+# CORS Middleware for frontend access
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
-    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
-@app.get("/transcribe")
-def get_transcription():
-    text = transcribe_user_question()
-    return {"question": text}
+# Register API routers
+app.include_router(upload_router)
+app.include_router(transcribe_router)
 
 if __name__ == "__main__":
+    # running the app on local host
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
